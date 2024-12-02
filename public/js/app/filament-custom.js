d98761dc6cf8be7cb2834d2f30443b4bf781ca79
Roberto Di Marco <robertox85@users.noreply.github.com>
2024-12-02 18:59:38 +0100

    .

---
+++ public/js/app/filament-custom.js
@@ -0,0 +1,6 @@
+// public/js/filament-custom.js
+console.log('Filament Custom JS caricato!');
+window.connectGoogle = function () {
+    alert('Connetti Google cliccato!');
+    // Chiamata AJAX o logica personalizzata
+};
